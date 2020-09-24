47e32c059d018f57dd5f9e5a4cfa17c41642592e
Yosuke Kobayashi <yosuke.kobayashi@ipmu.jp>
2020-09-24 20:27:04 +0900

    added a likelihood for linear redshift-space power spectrum.

---
+++ montepython/likelihoods/linear_power_fs/__init__.py
@@ -0,0 +1,56 @@
+from montepython.likelihood_class import Likelihood
+import numpy as np
+from mpi4py import MPI
+import logging
+import os, sys, time
+sys.path.append('/raid/kobayashi/mcmc')
+import boss_dr12_power_fs_likelihood as utils
+
+class linear_power_fs(Likelihood):
+    def __init__(self, path, data, command_line):
+        print('path:', path)
+        Likelihood.__init__(self, path, data, command_line)
+
+        k = np.load(os.path.join(self.data_directory, self.k_file))
+        signal = np.load(os.path.join(self.data_directory, self.signal_file))
+        cov = np.load(os.path.join(self.data_directory, self.cov_file))
+        self.config = utils.load_config(self.config_file)
+        k,data_vec,cov_mat = utils.load_data(k,signal,cov,self.config)
+        print('data loaded.')
+        self.like = utils.likelihood(k,data_vec,cov_mat,self.config,self.config['parameter_values'])
+        self.like.calc_invcov(Hartlap=False)
+
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        dirname_log = os.path.join(command_line.folder,'log_linear_power_fs')
+        print('initialize rank=%s' % rank)
+        if rank == 0:
+            if not os.path.exists(dirname_log):
+                os.makedirs(dirname_log)
+        logging.basicConfig(filename=os.path.join(dirname_log, 'log_' + '.%s' % rank), level=logging.INFO)
+
+    def loglkl(self, cosmo, data):
+        # We do not use 'cosmo', since we use the emulator to compute cosmologial quantities.
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        print('compute loglkl at rank=%s' % rank)
+
+        theta = []
+        for param in self.config['parameters']:
+            if not param['do_sample']: continue
+            if param['nspecies'] == 1: theta.append(data.mcmc_parameters[param['name']]['current'])
+            else:
+                for i in range(param['nspecies']):
+                    theta.append(data.mcmc_parameters[param['name']+'_%s' % i]['current'])
+        theta = np.array(theta)
+        logging.info('theta = %s' % theta)
+
+        ti = time.time()
+        lnprob, sigma8 = self.like.get_lnprob(theta)
+        tf = time.time()
+
+        data.mcmc_parameters['Omega_m']['current'] = 1.-data.mcmc_parameters['Omega_de']['current']
+        data.mcmc_parameters['sigma_8']['current'] = sigma8
+
+        print('elapsed time to compute loglkl at rank=%s: %s sec' % (rank, (tf-ti)))
+        return lnprob
